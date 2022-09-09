25b6b3b6dca556d7df106beb84b46652c7c1f907
sea <Estepanov77@gmail.com>
2022-09-09 13:42:16 +0300

    add method onCell in class FieldDrawer

--- src/FieldDrawer.java
+++ src/FieldDrawer.java
@@ -1,12 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class FieldDrawer extends JPanel {
+public class FieldDrawer extends JPanel implements MouseMotionListener, MouseListener {
     PointCell[][] field;
+    int mouseX = 0;
+    int mouseY = 0;
     FieldDrawer(PointCell[][] field)
     {
+        addMouseMotionListener(this);
+        addMouseListener(this);
         this.field = field;
         setBackground(Color.PINK);
+    }
+    private PointCell onCell (MouseEvent e)
+    {
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++) {
+                if (((e.getX() >= i * PointCell.cellSizeX) & (e.getX() <= i * PointCell.cellSizeX + PointCell.cellSizeX)) &
+                        ((e.getY() >= j * PointCell.cellSizeY) & (e.getY() <= j * PointCell.cellSizeY + PointCell.cellSizeY)))
+                    return field[j][i];
+            }
+        return null;
     }
 
     @Override
@@ -24,10 +41,10 @@ public class FieldDrawer extends JPanel {
                         g2.drawLine(PointCell.cellSizeX, 0, 0, PointCell.cellSizeY);
                         break;
                     case miss:
-                        g2.setColor(Color.BLUE);
+                        g2.setColor(new Color(50, 50, 255));
                         g2.fillRect(PointCell.cellSizeX*i, PointCell.cellSizeY*j, PointCell.cellSizeX, PointCell.cellSizeY);
                         g2.setColor(Color.RED);
-                        g2.fillOval(PointCell.cellSizeX / 3, PointCell.cellSizeY / 3, PointCell.cellSizeX / 2, PointCell.cellSizeY / 2);
+                        g2.fillOval(PointCell.cellSizeX*i + PointCell.cellSizeX/3, PointCell.cellSizeY*j+ PointCell.cellSizeY/3, PointCell.cellSizeX / 2, PointCell.cellSizeY / 2);
                         break;
                     case ship:
                         g2.setColor(Color.GRAY);
@@ -48,7 +65,54 @@ public class FieldDrawer extends JPanel {
                 }
                 g2.setColor(Color.BLACK);
                 g2.drawRect(PointCell.cellSizeX*i, PointCell.cellSizeY*j, PointCell.cellSizeX, PointCell.cellSizeY);
+                g2.drawString(String.valueOf(mouseX) + " " + String.valueOf(mouseY),100, 100);
             }
         }
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        if ((onCell(e) != null) & (onCell(e).cellState == PointCell.state.water))
+            onCell(e).cellState = PointCell.state.waterSelected;
+        repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++) {
+                if (((e.getX() >= i*PointCell.cellSizeX) & (e.getX() <= i*PointCell.cellSizeX+PointCell.cellSizeX)) &
+                        ((e.getY() >= j*PointCell.cellSizeY) & (e.getY() <= j*PointCell.cellSizeY+PointCell.cellSizeY))) {
+                    field[j][i].cellState = PointCell.state.miss;
+                    mouseX = e.getX();
+                    mouseY = e.getY();
+                }
+                repaint();
+            }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
